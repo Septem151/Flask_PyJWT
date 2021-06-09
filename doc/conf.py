65f8f8fa026e04093d93f13c44cb178e77917263
Carson Mullins <carsonmullins@yahoo.com>
2021-06-08 23:35:39 -0500

    Bump version to v0.1.6

--- doc/conf.py
+++ doc/conf.py
@@ -24,7 +24,7 @@ copyright = "2021, Carson Mullins"
 author = "Carson Mullins"
 
 # The full version, including alpha/beta/rc tags
-release = "0.1.5"
+release = "0.1.6"
 
 
 # -- General configuration ---------------------------------------------------
