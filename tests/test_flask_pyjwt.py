a4df428d05daaf39772aba9f944146909a62f961
Carson Mullins <carsonmullins@yahoo.com>
2021-06-08 21:21:27 -0500

    Add missing cryptography dependency for RSA encrypted JWTs

--- tests/test_flask_pyjwt.py
+++ tests/test_flask_pyjwt.py
@@ -3,4 +3,4 @@ from flask_pyjwt import __version__
 
 def test_version():
     """Assert that the version of flask_pyjwt matches the expected version."""
-    assert __version__ == "0.1.3"
+    assert __version__ == "0.1.4"
